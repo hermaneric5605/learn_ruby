b5808c02f045f746c5149d925de4a11d2e0b7287
Eric Herman <hermaneric@outlook.com>
2016-10-04 10:33:53 -0400

    Almost finished  02Calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,46 @@
 #write your code here
+def add (num1, num2)
+  num1 + num2
+end
+
+def subtract (num1, num2)
+	num1 - num2
+end
+
+
+
+
+def sum(array)
+  sum = 0
+  array.each do |i|
+    sum += i
+  end
+  return sum
+end
+
+def multiply(array)
+  result = 1
+  array.each do |i|
+    result *= i
+  end
+  return result
+end
+
+def power(base,power)
+  result = 1
+  while (power > 0) do
+    result *= base
+    power -= 1
+  end
+  return result
+end
+
+
+def factorial(num)
+  result = 1
+  num.times do |i|
+    result *= (i+1)
+  end
+  result
+
+end
